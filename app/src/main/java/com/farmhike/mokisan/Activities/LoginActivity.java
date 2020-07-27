7e83bb9692c2a10abf54d4371d98f95975d74a9b
Ashish <kumarashish9803@gmail.com>
2020-07-27 22:02:00 +0530

    Design Updated

--- app/src/main/java/com/farmhike/mokisan/Activities/LoginActivity.java
+++ app/src/main/java/com/farmhike/mokisan/Activities/LoginActivity.java
@@ -3,28 +3,63 @@ package com.farmhike.mokisan.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.farmhike.mokisan.Models.UserLoginData;
 import com.farmhike.mokisan.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button next_login;
+    TextInputLayout editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        next_login = (Button)(findViewById(R.id.next_login));
+        editText = (TextInputLayout)(findViewById(R.id.editText_Login_phoneNo));
+
+        next_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatePhoneNo();
+            }
+        });
     }
 
-    public void sendOtp(View view) {
-        validatePhoneNo();
-        startActivity(new Intent(this, VerifyOtp.class));
+    public void sendOtp() {
+        Intent i = new Intent(LoginActivity.this,VerifyOtp.class);
+        startActivity(i);
     }
 
     private void validatePhoneNo() {
-        EditText editText = findViewById(R.id.editText_Login_phoneNo);
-        String phoneNo = String.valueOf(editText.getText());
+        //EditText editText = findViewById(R.id.editText_Login_phoneNo);
+        String phoneNo = editText.getEditText().getText().toString();
+
+        if(phoneNo.length()==0 || phoneNo.length()>10)
+        {
+            Toast.makeText(getApplicationContext(),"Please Enter Your Phone Number",Toast.LENGTH_LONG).show();
+        }
+        else if(!phoneNo.matches("[0-9]{10}"))
+        {
+            editText.setError("Enter 10 digit mobile number");
+        }
+        else
+        {
+            UserLoginData.getInstance().setPhoneNo(phoneNo);
+            sendOtp();
+        }
+
         /*
          *  if everything fine
          *
--- app/src/main/java/com/farmhike/mokisan/Activities/SplashScreen.java
+++ app/src/main/java/com/farmhike/mokisan/Activities/SplashScreen.java
@@ -1,16 +1,49 @@
 package com.farmhike.mokisan.Activities;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.farmhike.mokisan.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation top,bottom;
+    ImageView logo;
+    TextView name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
+
+        top = AnimationUtils.loadAnimation(this,R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this,R.anim.bottom);
+
+        logo = findViewById(R.id.logo);
+        name = findViewById(R.id.name);
+
+        logo.setAnimation(top);
+        name.setAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.swipe_right,R.anim.swipe_left);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
