1bc53b6bd6a4d472eecc5954e5fa8e6234682935
ashish9803 <kumarashish9803@gmail.com>
2020-09-12 16:36:05 +0530

    Added userprofile, cart and home

--- app/src/main/java/com/farmhike/mokisan/Activities/UserProfile.java
+++ app/src/main/java/com/farmhike/mokisan/Activities/UserProfile.java
@@ -1,6 +1,9 @@
 package com.farmhike.mokisan.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,22 @@ import com.farmhike.mokisan.R;
 
 public class UserProfile extends AppCompatActivity {
 
+    ImageView back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        back = (ImageView)(findViewById(R.id.back));
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UserProfile.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
